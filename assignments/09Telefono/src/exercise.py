a65d2c78d9845d28a9ce8466d1b7abe8d26a096c
A01412662 <A01412662@itesm.mx>
2021-08-26 22:00:24 -0500

    Tarea de "Teléfono" Realizada

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    mensajes = int (input("Dame el número de mensajes: "))
+    megas = float (input("Dame el número de megas: "))
+    minutos = int (input("Dame el número de minutos: "))
+
+    costo = (mensajes*.80)+(megas*.80)+(minutos*.80)
+    print("El costo mensual es:",costo)
+
+
 
 
 if __name__ == '__main__':
