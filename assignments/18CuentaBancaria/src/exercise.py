c396e0a65c121db4ceb190eb92e5a328116b48c4
A01412662 <A01412662@itesm.mx>
2021-08-26 23:07:59 -0500

    Tarea de "CuentaBancaria" Realizada

--- assignments/18CuentaBancaria/src/exercise.py
+++ assignments/18CuentaBancaria/src/exercise.py
@@ -1,7 +1,14 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    mes_anterior = float (input("Dame el saldo del mes anterior: "))
+    ingresos = float (input("Dame los ingresos: "))
+    egresos = float (input("Dame los egresos: "))
+    cheques = int (input("Dame el número de cheques: "))
 
+    sin_interes = (mes_anterior + ingresos - egresos - (cheques * 13))
+    con_interes = (sin_interes) - (sin_interes * .075)
+
+    print("El saldo final de la cuenta es:",con_interes)
 
 if __name__ == '__main__':
     main()
