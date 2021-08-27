b1754c101492866f526ab540cd9ebfcbb8702491
A01412662 <A01412662@itesm.mx>
2021-08-27 00:06:50 -0500

    Tarea de "Editorial" Realizada

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,6 +1,20 @@
+from typing import TYPE_CHECKING
+
+
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    num_paginas = int (input("Dame el número de palabras : "))
+
+    costo = ((num_paginas / 475) * 60)+ 60
+    costo_variaspag = costo - (costo * .10)
+    costo_unapag = 60- (60*.10)
+
+
+    print ("El costo de la publicación es:",costo_variaspag)
+
+
+
+
 
 
 if __name__ == '__main__':
